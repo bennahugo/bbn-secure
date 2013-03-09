5c212c569acc2758ae02251b58a1822121b37b1a
bennahugo@gmail.com <bennahugo@gmail.com@11ded1f4-5bcd-194d-2c6e-0cee0459935e>
2013-03-09 23:16:25 +0000

    added last bit of timestamp exchange

--- SecureClientServer/src/KeyringReader.java
+++ SecureClientServer/src/KeyringReader.java
@@ -1,11 +1,34 @@
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.math.BigInteger;
+import java.security.spec.RSAPrivateKeySpec;
+import java.security.spec.RSAPublicKeySpec;
 import java.util.HashMap;
 
 
 public class KeyringReader {
-	private HashMap<String,Pair<BigInteger,BigInteger>> keys;
+	private HashMap<String,RSAPublicKeySpec> keys;
 	
-	public KeyringReader(String filename){
-		keys = new HashMap<String,Pair<BigInteger,BigInteger>>();
+	public KeyringReader(String filename) throws FileNotFoundException, IOException{
+		keys = new HashMap<String,RSAPublicKeySpec>();
+		ObjectInputStream oin = new ObjectInputStream(
+				new BufferedInputStream(new FileInputStream(filename)));
+		try {
+			while (oin.available() > 0){
+				String name = (String) oin.readObject();
+				BigInteger pubMod = (BigInteger) oin.readObject(), 
+						pubExp = (BigInteger) oin.readObject();
+				RSAPublicKeySpec publ = new RSAPublicKeySpec(pubMod, pubExp);
+				keys.put(name, publ);
+			}
+		} catch (Exception e) {
+		    throw new IOException("Unexpected error", e);
+		} finally {
+		    oin.close();
+		}
+		
 	}
 }
